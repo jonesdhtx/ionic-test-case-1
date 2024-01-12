65074bc558a9ac9bcec6457e2539bfa5d80391b4
David Jones <jonesdhtx@gmail.com>
2024-01-11 19:39:49 -0600

    Recreate issue for this ionic bug submission

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,78 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React, { useRef } from 'react';
+import {
+  IonButtons, IonButton, IonHeader, IonContent, IonToolbar, IonTitle, IonPage, useIonModal, IonPopover,
+} from '@ionic/react';
 
-const Home: React.FC = () => {
+const ModalExample = ({
+  onDismiss,
+}: {
+  onDismiss: (data?: string | null | undefined | number, role?: string) => void;
+}) => {
+  const inputRef = useRef<HTMLIonInputElement>(null);
+
+  function onPopoverItemClickFromModal (ev: React.MouseEvent) {
+    console.log('onPopoverItemClick From Modal!!')
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonButtons slot="start">
+            <IonButton color="medium" onClick={() => onDismiss(null, 'cancel')}>
+              Cancel
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Welcome</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonButton id="click-trigger-2">Trigger Popover</IonButton>
+        <IonPopover dismissOnSelect trigger="click-trigger-2" triggerAction="click">
+          <IonContent class="ion-padding">
+            Hello World!
+            <IonButton expand="block" onClick={onPopoverItemClickFromModal}>I'm clickable</IonButton>
+          </IonContent>
+        </IonPopover>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Home;
+function Example() {
+  const [present, dismiss] = useIonModal(ModalExample, {
+    onDismiss: (data: string, role: string) => dismiss(data, role),
+  });
+
+  function onPopoverItemClickFromPage(ev: React.MouseEvent) {
+    console.log('onPopoverItemClick From Page!!')
+  }
+
+  function openModal() {
+    present();
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Popover within a Controller Modal</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonButton expand="block" onClick={() => openModal()}>
+          Open Modal
+        </IonButton>
+        <IonButton id="click-trigger-1">Trigger Popover</IonButton>
+        <IonPopover dismissOnSelect trigger="click-trigger-1" triggerAction="click">
+          <IonContent class="ion-padding">
+            Hello World!
+            <IonButton expand="block" onClick={onPopoverItemClickFromPage}>I'm clickable</IonButton>
+          </IonContent>
+        </IonPopover>
+      </IonContent>
+    </IonPage>
+  );
+}
+
+export default Example;
